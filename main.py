920603a4ca0118a64f1ae8a2e651eb354a909443
justin <jjzwink@gmail.com>
2019-11-18 21:21:47 -0500

    1st push

---
+++ main.py
@@ -0,0 +1,32 @@
+import pygame
+import maps
+import towers
+import enemys
+
+#Main game init and screen Res setup
+pygame.init()
+size = WIDTH, Height = (1024, 764)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Zwink's Tower Defence")
+mainicon = pygame.image.load("icons/medieval-tower.png")
+pygame.display.set_icon(mainicon)
+
+dead = False
+
+#Global Vars
+
+FPS = 15
+FPSCLOCK = pygame.time.Clock()
+
+#Main Loop
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            dead = True
+
+    maps.map_1()
+
+    pygame.display.flip()
+   
